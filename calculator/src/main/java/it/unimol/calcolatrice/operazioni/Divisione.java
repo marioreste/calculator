eb56fd8b508a8efdfcb3529b682192d7f25140fc
marioreste <mariogallozzi@gmail.com>
2025-02-12 11:56:40 +0100

    Added exception in case of division by zero

--- calculator/src/main/java/it/unimol/calcolatrice/operazioni/Divisione.java
+++ calculator/src/main/java/it/unimol/calcolatrice/operazioni/Divisione.java
@@ -2,6 +2,9 @@ package it.unimol.calcolatrice.operazioni;
 
 public class Divisione {
     public double calcola(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Errore: Divisione per zero non consentita.");
+        }
         return (a / b);
     }
 }
