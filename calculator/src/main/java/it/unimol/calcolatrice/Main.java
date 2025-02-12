1b9e1afff55248540a51fa1677be3fda7179d810
marioreste <mariogallozzi@gmail.com>
2025-02-12 11:44:05 +0100

    Modified main

--- calculator/src/main/java/it/unimol/calcolatrice/Main.java
+++ calculator/src/main/java/it/unimol/calcolatrice/Main.java
@@ -1,7 +1,38 @@
 package it.unimol.calcolatrice;
 
+import it.unimol.calcolatrice.operazioni.*;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci il primo numero: ");
+        double num1 = Double.parseDouble(scanner.nextLine());
+
+        System.out.print("Inserisci l'operazione (+ o -): ");
+        char operazione = scanner.next().charAt(0);
+
+        System.out.print("Inserisci il secondo numero: ");
+        double num2 = Double.parseDouble(scanner.nextLine());
+
+        double risultato = 0;
+
+        switch (operazione) {
+            case '+':
+                risultato = new Addizione().calcola(num1, num2);
+                break;
+            case '-':
+                risultato = new Sottrazione().calcola(num1, num2);
+                break;
+            default:
+                System.out.println("Operazione non valida.");
+                scanner.close();
+                return;
+        }
+
+        System.out.println("Risultato: " + risultato);
+        scanner.close();
     }
 }
