c542f8c8fac74c50934abba2d53a45814b36203c
marioreste <mariogallozzi@gmail.com>
2025-02-12 18:07:42 +0100

    Changed DivisioneTest

--- calculator/src/test/java/it/unimol/calcolatrice/operazioni/DivisioneTest.java
+++ calculator/src/test/java/it/unimol/calcolatrice/operazioni/DivisioneTest.java
@@ -6,5 +6,7 @@ class DivisioneTest {
 
     @org.junit.jupiter.api.Test
     void calcola() {
+        Divisione divisione = new Divisione();
+        assertEquals(5, divisione.calcola(35, 7));
     }
 }
